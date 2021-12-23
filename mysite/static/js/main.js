11bd3e2d9af2c1cf0bbba3c6d515393ac177cf48
Arc1el <khm970514@gmail.com>
2021-12-23 15:43:44 +0900

    chat service

--- mysite/static/js/main.js
+++ mysite/static/js/main.js
@@ -148,11 +148,28 @@ var userMedia = navigator.mediaDevices.getUserMedia(constraints)
     });
 
 var btnSendMsg = document.querySelector('#btn-send-msg');
+var messageList = document.querySelector('#message-list');
+var messageInput = document.querySelector('#msg');
 
 btnSendMsg.addEventListener('click', sendMsgOnClick);
 
 function sendMsgOnClick(){
-    
+    var message = messageInput.value;
+
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode('Me: ' + message));
+
+    messageList.appendChild(li);
+
+    var dataChannels = getDataChannels();
+
+    message = username + ': ' + message;
+
+    for(index in dataChannels){
+        dataChannels[index].send(message);
+    }
+
+    messageInput.value = '';
 }
 
 function sendSignal(action, message){
@@ -286,7 +303,6 @@ function addLocalTracks(peer){
     return;
 }
 
-var messageList = document.querySelector('#message-list');
 function dcOnMessage(event){
     var message = event.data;
 
@@ -327,4 +343,16 @@ function removeVideo(video){
     var videoWrapper = video.parentNode;
 
     videoWrapper.parentNode/removeChild(videoWrapper);
+}
+
+function getDataChannels(){
+    var dataChannels = [];
+    
+    for(peerUsername in mapPeers){
+        var dataChannel = mapPeers[peerUsername][1];
+
+        dataChannels.push(dataChannel);
+    }
+
+    return dataChannels;
 }

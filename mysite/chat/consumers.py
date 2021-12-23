a83474063852dd454b00b76c1aed1ec3f1c883bf
Arc1el <khm970514@gmail.com>
2021-12-23 13:11:21 +0900

    create and setting (chat, mysite, static... etc)

---
+++ mysite/chat/consumers.py
@@ -0,0 +1,9 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        
+    
+    async def disconnect(self):
+
+    async def receive(self):
